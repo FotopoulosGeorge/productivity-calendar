eb3a456acc138b8d98dc5f04686c162227ca41be
unknown <fotopoulos.g.j@gmail.com>
2025-06-05 09:33:02 +0300

    Added app icon

--- electron/main.js
+++ electron/main.js
@@ -15,6 +15,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, '../icons/icon.png'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
